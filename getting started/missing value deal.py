3ac330eabffb546650bcc13b71652a093de26b8b
ashutoshdutt-9211 <ashutoshdutt9211@gmail.com>
2019-01-02 23:17:27 +0530

    done

--- getting started/missing value deal.py
+++ getting started/missing value deal.py
@@ -5,7 +5,7 @@
     1. The Simple One: dropping the missing value containing column
     2. Simple Imputer: filling missing values with the mean value
 '''
-' Dropping the missing value column'
+' Dropping the missing value column and calculating MAE'
 def drop(data):
     column_with_missing_values= [col for col in data.columns if data[col].isnull().any()]
     data=data.drop(column_with_missing_values,axis=1)
@@ -32,7 +32,13 @@ from sklearn.metrics import mean_absolute_error as mae# : for evaluation of maod
 data= pd.read_csv('melb_data.csv')
 #print(data.describe()) to just get a confirmation and overview of loaded data
 #print(data.columns) : to get look on columns in data set
-data= data.dropna(axis=0) #:to drop missing values
+#data= data.dropna(axis=0) #:to drop missing values
+
+
+
+'''Dropping the missing value'''
+from sklearn.model_selection import train_test_split as tts
+
 data=drop(data)
 
 """choosing features for predictions"""
@@ -40,24 +46,18 @@ y= data.Price  #: choosing output features
 input_feature=['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 X= data[input_feature]
 #print(X.head()): to check if data is loaded or not
-
-
-'''Splitting the data to get more accurate model'''
-from sklearn.model_selection import train_test_split as tts
 train_X,test_X,train_y,test_y= tts(X,y,random_state=0)
-print(rfrModel(train_X,test_X,train_y,test_y))
+print("error using dropiing the value",rfrModel(train_X,test_X,train_y,test_y))
 
 
-"""choosing features for predictions"""
+"""Using imputer to deal mission value"""
+data= pd.read_csv('melb_data.csv')
 y= data.Price  #: choosing output features
 input_feature=['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
-X= imputation(data[input_feature])
+new_X= imputation(data[input_feature])
 #print(X.head()): to check if data is loaded or not
 
-
-'''Splitting the data to get more accurate model'''
-
-train_X,test_X,train_y,test_y= tts(X,y,random_state=0)
-print(rfrModel(train_X,test_X,train_y,test_y))
+train_X,test_X,train_y,test_y= tts(new_X,y,random_state=0)
+print('error using Imputer: ',rfrModel(train_X,test_X,train_y,test_y))
     
     
